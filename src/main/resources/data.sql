ad6d31c02c5a7d8e41ed68dec2bfab0f9efe37bb
12816 <aybolek27@gmail.com>
2022-07-31 12:20:38 -0500

    task service work

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,5 +11,5 @@ values ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, true, 'admin'
 
 insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
                   first_name, gender, last_name, user_name, role_id)
-values ('2022-01-05 00:00:00', 1, false, '2022-01-05 00:00:00', 1, true, 'manager', 'MALE', 'manager', 'admin@manager.com',
+values ('2022-01-05 00:00:00', 2, false, '2022-01-05 00:00:00', 2, true, 'manager', 'MALE', 'manager', 'admin@manager.com',
         2);
